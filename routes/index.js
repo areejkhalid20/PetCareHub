731eb3fd4672788d39ae9f3996ffb097f68875b6
Areej Khalid <123973877+areejkhalid20@users.noreply.github.com>
2024-05-04 14:55:04 +0500

    added a page to display petfood (#11)

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,10 @@ router.get('/sign_in', (req, res) => {
 router.get('/sign_up', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'sign_up.html'));
 });
+
+router.get('/petFood', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'petFood.html'));
+});
+
+
 module.exports = router;
